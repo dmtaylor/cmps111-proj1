0b2731051acfb6b9961543fc28ad467ebd052948
David Taylor <dmtaylor2011@gmail.com>
2014-04-08 23:08:36 -0700

    Worked on parsing command line input. Yet untested.

--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
 #include <signal.h>
 #include <errno.h>
 
@@ -15,20 +16,61 @@ extern char **get_line(void);
 
 int main(int argc, char *argv[]) {
   int i, rarg_pos;
-  char redir_out, redir_in, bkg;
+  char redir_out, redir_in, bkg, parse_err, in_call;
   char** args;
-  rarg_pos = 0;
-  redir_out = 0;
-  redir_in = 0;
-  bkg = 0; 
 
   while(1) {
-    printf{"\ndsh$");
+    redir_out = 0;
+    redir_in = 0;
+    bkg = 0;
+    rarg_pos = 0;
+    parse_err = 0;
+    in_call = 1;
+    printf("\ndsh$");
     args = get_line();
     if(!args) continue;
     if(strcmp(args[0],"exit") == 0) break;
     for(i = 0; args[i] != NULL; i++) {
-      /*printf("Argument %d: %s\n", i, args[i]);*/
+      if(args[i]=='<'){
+        if(args[i+1] == NULL){
+          fprintf(stderr, "\nError: no file given to <");
+          parse_err = 1;
+          break;
+        }
+        redir_in = 1;
+        /* file redir in here */
+        
+      }
+      if(args[i]=='>'){
+        if(args[i+1] == NULL){
+          fprintf(stderr, "\nError: no file given to >");
+          parse_err = 1;
+          break;
+        }
+        redir_out = 1;
+        /* file redir out here */
+        
+      }
+      if(args[i] == '&'){
+        bkg = 1;
+        in_call = 0;
+      }
+      if(in_call) rarg_pos++;
+    }
+    if(parse_err) continue;
+
+    pid_t proc;
+
+    proc = fork();
+    if(proc = -1){
+      fprintf(stderr, "\nFork failed, got %s\n",strerror(errno));
+      exit(2);
+    }
+    else if(proc = 0){
+      /* run child process here */
+    }
+    else {
+      /* do parent here */
     }
   }
   return 0;
