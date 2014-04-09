88087048e65c336ffbdff23670c146dfd67ba946
David Taylor <dmtaylor2011@gmail.com>
2014-04-09 16:24:23 -0700

    Updated version of scanner

--- lex.c
+++ lex.c
@@ -1,24 +1,25 @@
 %option noyywrap
 
 %{
+#undef _POSIX_SOURCE
+#define _POSIX_SOURCE
 #define YY_PROTO(proto) proto
 #define YY_DECL char **yylex YY_PROTO(( void ))
 int _numargs = 10;
 char *_args[10];
 int _argcount = 0;
+int readInputForLexer(char *buffer, int *numBytesRead, int maxBytesToRead);
 
 #include <stdio.h>
+int fileno(FILE *);
 #include <stdlib.h>
 #include <string.h>
-char *strdup(char* src)
-{
-    char *dst = malloc(strlen(src)+1);
-    if(dst) strcpy(dst, src);
-    return dst;
-}
+
+#undef YY_INPUT
+#define YY_INPUT(b,r,s) readInputForLexer(b,&r,s)
 %}
 
-WORD	[a-zA-Z0-9\/\.-]+
+WORD	[a-zA-Z0-9\/\.\-\=]+
 SPECIAL	[()><|&;*]
 
 %%
@@ -40,6 +41,27 @@ SPECIAL	[()><|&;*]
 
 %%
 
-char **get_line() {
+char **get_line(void) {
   return (char **)yylex();
+}
+
+int readInputForLexer(char *buffer, int *numBytesRead, int maxBytesToRead)
+{
+    do {
+        int c = getc(stdin);
+        if (c == EOF) {
+            /* EOF indicates either EOF or error */
+            if (feof(stdin)) {
+                *numBytesRead = YY_NULL;
+                return 0;
+            }
+            /* Must have been a signal interrupting us */
+            clearerr(stdin);
+            
+        } else {
+            buffer[0] = c;
+            *numBytesRead = 1;
+            return 0;
+        }
+    } while(1);
 }
