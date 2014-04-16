32aed9014545adf1d2ac971651bc8d5922d292f4
David Taylor <dmtaylor2011@gmail.com>
2014-04-16 01:29:48 -0700

    Added test I used heavily in testing

---
+++ test_1.c
@@ -0,0 +1,14 @@
+/* Test program 1 : test background processes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv){
+    sleep(20);
+    printf("I waited 20 secs. exiting 0\n");
+    return 0;
+}
+
